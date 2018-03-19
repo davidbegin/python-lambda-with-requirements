d688a5f06746ef55b6517e861302cab79b39d1f3
David Begin <davidmichaelbe@gmail.com>
2018-03-19 10:49:22 -0700

    Trigger updates on changes to requirements.in file

--- main.tf
+++ main.tf
@@ -16,6 +16,7 @@ data "archive_file" "lambda_archive" {
 resource "null_resource" "create_lambda_zip" {
   triggers = {
     lambda_code = "${sha1(file("${local.function_folder}/${var.function_filename}.py"))}"
+    lambda_code = "${sha1(file("${local.function_folder}/requirements.in"))}"
   }
 
   provisioner "local-exec" {
